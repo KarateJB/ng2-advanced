d4dac746fadba45b77f8f6859b16e48e13878be6
KarateJB <ppippenkimo@gmail.con>
2017-02-12 11:07:17 +0800

    Route sample.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { Page2Component } from './page2/page2.component';
+
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home',component: LayoutComponent},
+  {path: 'page2',component: Page2Component},
+
+  //The uri which not match any will be math this one, only apply this when go live on production.
+  {path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing:true})],
   exports: [RouterModule],
   providers: []
 })
