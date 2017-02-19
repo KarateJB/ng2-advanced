9ea1aa388c7aa2a0f35dcb93c1d3f715ab3466d1
KarateJB <ppippenkimo@gmail.con>
2017-02-19 12:13:38 +0800

    Dynamic form controls!

--- src/app/reactiveform/reactiveform.component.ts
+++ src/app/reactiveform/reactiveform.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -8,9 +8,11 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class ReactiveFormComponent implements OnInit {
 
-  form: FormGroup;
+  private form: FormGroup;
+  private dynamicForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
+
     this.form = this.fb.group({
       title: 'This is title',
       'name': this.fb.group({
@@ -18,16 +20,30 @@ export class ReactiveFormComponent implements OnInit {
         lastName: 'Lin'
       })
     });
+
+
+    this.dynamicForm = this.fb.group({
+      title: 'This is title',
+      'name': this.fb.array([
+        this.fb.control('JB1', Validators.required),
+        this.fb.control('JB2', Validators.required),
+        this.fb.control('JB3', Validators.required)
+      ])
+    });
   }
 
   getFieldInvalid(fieldName) {
     // return this.form.controls[fieldName].invalid;
-    let nameGroup: FormGroup  = <FormGroup>this.form.controls["name"];
+    let nameGroup: FormGroup = <FormGroup>this.form.controls["name"];
     return nameGroup.controls[fieldName].invalid;
 
   }
 
   ngOnInit() {
+
+    let groupAry: FormArray = <FormArray>this.dynamicForm.controls["name"];
+    groupAry.insert(groupAry.length, this.fb.control('JB4'));
+
   }
 
 }
